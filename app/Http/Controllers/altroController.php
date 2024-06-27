a4adfa97f63a283782a00075e1036fda9b331c12
MatteoZattini <matteozattini@hotmail.com>
2024-06-28 00:58:19 +0200

    BONUS extra connected to database and printed teachers's info

--- app/Http/Controllers/altroController.php
+++ app/Http/Controllers/altroController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class altroController extends Controller
 {
     public function index(){
-        return view('altro');
+        $teachers = DB::table('teachers')->select('name', 'surname', 'phone', 'email', 'office_address')->get();
+        return view('altro')->with('teachers', $teachers);
     }
 }
--- resources/views/altro.blade.php
+++ resources/views/altro.blade.php
@@ -3,10 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
     <h1>altra pagina</h1>
     <a href="/">Welcome</a>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex flex-wrap">
+                @foreach ($teachers as $teacher)
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title">{{ $teacher->surname }}</h5>
+                      <h6 class="card-subtitle mb-2 text-body-secondary">{{ $teacher->name }}</h6>
+                      <p class="card-text">{{ $teacher->email }} <br>
+                        {{ $teacher->phone }} <br>
+                        {{ $teacher->office_address }} <br>
+                    </p>
+                    </div>
+                  </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 </body>
 </html>
